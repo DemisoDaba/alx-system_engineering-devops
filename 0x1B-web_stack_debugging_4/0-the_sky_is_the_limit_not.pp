aa7e1b008e65006e5fe81f01db2865fbd73583c3
Demisodaba <demo.nkmt1@gmail.com>
2023-11-18 17:42:53 +0300

    push

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,22 @@
 # Fix nginx to many failed requests
-exec {'increase_workers':
-command => '/bin/sed -i "s/worker_processes 4/worker_processes 8/g" /etc/nginx/nginx.conf',
-notify  => Service['nginx'],
+# Modify Nginx configuration using Puppet resources
+file { '/etc/nginx/nginx.conf':
+  ensure  => present,
+  content => template('module_name/nginx.conf.erb'), # Use a template file for configuration
+  notify  => Service['nginx'],
 }
 
-service {'nginx':
-ensure => 'running',
+# Adjust worker_processes if needed
+if $::nginx::params::worker_processes != 8 {
+  class { 'nginx':
+    worker_processes => 8,
+  }
 }
+
+# Ensure Nginx service is running and configured
+service { 'nginx':
+  ensure  => 'running',
+  enable  => true,
+  require => File['/etc/nginx/nginx.conf'],
+}
+
